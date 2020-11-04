2b97fc3eaad04ac4e9abcd0c1bdd9e90f2a352d9
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2020-11-04 23:40:23 +0545

    Add named urls

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import JokesList, JokeDetail, JokeRandom
 
 urlpatterns = [
-    path("", JokesList.as_view()),
-    path("<int:pk>/", JokeDetail.as_view()),
-    path("random/", JokeRandom.as_view())
+    path("", JokesList.as_view(), name="jokes_list"),
+    path("<int:pk>/", JokeDetail.as_view(), name="joke_detail"),
+    path("random/", JokeRandom.as_view(), name="random_joke")
 ]
