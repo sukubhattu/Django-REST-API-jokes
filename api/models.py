fc293316be55213a15c9de887bb352a5fcb3d956
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2020-11-08 08:18:53 +0545

    Remove author field

--- api/models.py
+++ api/models.py
@@ -1,12 +1,8 @@
 from django.db import models
 
-# import default user model of django
-from django.contrib.auth.models import User
-
 
 class Jokes(models.Model):
     punchLine = models.TextField(max_length=500)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
     createdDate = models.DateField(auto_now_add=True)
     updatedDate = models.DateField(auto_now=True)
 
