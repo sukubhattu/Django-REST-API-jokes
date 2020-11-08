43c8a772e5127f108f13289cd1ec929fa0cd671b
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2020-11-08 08:17:24 +0545

    Remove author field

--- api/serializers.py
+++ api/serializers.py
@@ -6,4 +6,4 @@ from .models import Jokes
 class JokesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Jokes
-        fields = ["id", "punchLine", "author", "createdDate"]
+        fields = ["id", "punchLine", "createdDate"]
